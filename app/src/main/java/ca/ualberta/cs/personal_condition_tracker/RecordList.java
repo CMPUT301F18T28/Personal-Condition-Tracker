a2eba3f37252edfbd4de8aa5153a96ee1ff1e46e
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 17:48:01 -0600

    added methods

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/RecordList.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/RecordList.java
@@ -4,4 +4,29 @@ import java.util.ArrayList;
 
 public class RecordList {
     private ArrayList<Record> record_list;
+
+    RecordList() {
+        this.record_list = new ArrayList<Record>();
+    }
+
+    public boolean hasRecord(Record new_record) {
+        return this.record_list.contains(new_record);
+    }
+    public Record getRecord(int index){
+        return this.record_list.get(index);
+    }
+
+    public void addRecord(Record new_record) {
+        this.record_list.add(new_record);
+    }
+    public void deleteRecord(Record new_record) {
+        this.record_list.remove(new_record);
+    }
+    public void editRecord(int index, Record new_record) {
+        this.record_list.set(index, new_record);
+    }
+    public ArrayList<Record> queryByKeyword(String keyword) {
+        return new ArrayList<Record>();
+    }
+
 }
