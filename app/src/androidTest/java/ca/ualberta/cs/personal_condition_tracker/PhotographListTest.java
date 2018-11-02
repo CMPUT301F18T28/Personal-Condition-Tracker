ba3ca48327a1716c73c2f308cd384c2a179b6857
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 19:50:21 -0600

    adding test cases

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographListTest.java
@@ -1,5 +1,43 @@
+package ca.ualberta.cs.personal_condition_tracker;
+
 import static org.junit.Assert.*;
 
 public class PhotographListTest {
+
+    public void testHasRecord(){
+        RecordList new_record_list = new RecordList();
+        Record new_record = new Record("Title",  "I am a record.");
+        new_record_list.addRecord(new_record);
+        assertTrue(new_record_list.hasRecord(new_record));
+    }
+
+    public void testAddRecord() {
+        Record new_record = new Record("Title",  "I am a record.");
+        RecordList new_record_list = new RecordList();
+        new_record_list.addRecord(new_record);
+        assertTrue(new_record_list.hasRecord(new_record));
+    }
+
+    public void testDeleteRecord() {
+        RecordList new_record_list = new RecordList();
+        Record new_record = new Record("Title", "I am a record.");
+        new_record_list.addRecord(new_record);
+        new_record_list.deleteRecord(new_record);
+        assertFalse(new_record_list.hasRecord(new_record));
+    }
+    public void testGetRecord() {
+        Record new_record = new Record("Title",  "I am a record.");
+        RecordList new_record_list = new RecordList();
+        new_record_list.addRecord(new_record);
+        assertTrue(new_record_list.getRecord(0).equals(new_record));
+    }
+    public void testEditRecord() {
+        Record new_record = new Record("Title", "I am a record.");
+        RecordList new_record_list = new RecordList();
+        new_record_list.addRecord(new_record);
+        Record newer_record = new Record("Title",  "I am still a record.");
+        new_record_list.editRecord(0, newer_record);
+        assertTrue(new_record_list.getRecord(0).equals(newer_record));
+    }
 
 }
