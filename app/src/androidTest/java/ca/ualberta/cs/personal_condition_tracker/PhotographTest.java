c957c0a6f99ef0c29f6b80f650f53cd6381378da
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 20:26:10 -0600

    modifying test cases

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographTest.java
@@ -7,18 +7,4 @@ import java.util.Base64;
 
 public class PhotographTest extends TestCase {
 
-
-    public void testGetFilename() {
-        Photograph photo = new Photograph();
-        photo.setFilename("Photograph");
-        assertTrue(photo.getFilename().equals("Photograph"));
-    }
-    public void testSetFilename() {
-        Photograph photo = new Photograph("Photograph");
-        assertTrue(photo.getFilename().equals("Photograph"));
-        photo.setFilename("StillAPhotograph");
-        assertTrue(photo.getFilename().equals("StillAPhotograph"));
-    }
-    public void textGetThumbnail() {
-    }
 }
