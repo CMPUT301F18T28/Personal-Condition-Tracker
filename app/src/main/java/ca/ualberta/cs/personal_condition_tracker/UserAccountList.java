b0f1a1757c4c433528b7f88d9d9fe2850d095454
William Nichols <wcomhelp@users.noreply.github.com>
2018-11-01 18:21:52 -0600

    create addUserAccount() function
    Signed-off-by: William Nichols <wcomhelp@users.noreply.github.com>

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/UserAccountList.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/UserAccountList.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 
 public class UserAccountList {
     private ArrayList<UserAccount> user_accounts;
+
+    public void addUserAccount(UserAccount account_to_add) {
+        this.user_accounts.add(account_to_add);
+    }
+
 }
