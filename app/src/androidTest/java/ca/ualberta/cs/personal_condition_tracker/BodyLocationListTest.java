83305a460e7559539977dbc349b5e8b5c8523bb6
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-02 14:42:03 -0600

    adding methods, attributes, test cases for body location files
    Signed-off-by: Riley Voon <rcvoon@cs.ualberta.ca>

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/BodyLocationListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/BodyLocationListTest.java
@@ -1,5 +1,48 @@
+package ca.ualberta.cs.personal_condition_tracker;
+
+import org.junit.Test;
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-public class BodyLocationListTest {
+public class BodyLocationListTest extends TestCase{
 
+
+    public void testHasBodyLocation() {
+        BodyLocationList body_locations = new BodyLocationList();
+        BodyLocation body_location = new BodyLocation();
+        body_locations.addBodyLocation(body_location);
+        assertTrue(body_locations.hasBodyLocation(body_location));
+    }
+
+    public void testGetBodyLocation() {
+        BodyLocationList body_locations = new BodyLocationList();
+        BodyLocation body_location = new BodyLocation();
+        body_locations.addBodyLocation(body_location);
+        assertTrue(body_locations.getBodyLocation(0).equals(body_location));
+    }
+
+    public void testAddBodyLocation() {
+        BodyLocationList body_locations = new BodyLocationList();
+        BodyLocation body_location = new BodyLocation();
+        body_locations.addBodyLocation(body_location);
+        assertTrue(body_locations.hasBodyLocation(body_location));
+    }
+
+    public void testDeleteBodyLocation() {
+        BodyLocationList body_locations = new BodyLocationList();
+        BodyLocation body_location = new BodyLocation();
+        body_locations.addBodyLocation(body_location);
+        assertTrue(body_locations.hasBodyLocation(body_location));
+        body_locations.deleteBodyLocation(body_location);
+        assertFalse(body_locations.hasBodyLocation(body_location));
+    }
+
+    public void testEditBodyLocation() {
+        BodyLocationList body_locations = new BodyLocationList();
+        BodyLocation body_location = new BodyLocation();
+        body_locations.addBodyLocation(body_location);
+        BodyLocation new_body_location = new BodyLocation();
+        body_locations.editBodyLocation(0, new_body_location);
+        assertTrue(body_locations.getBodyLocation(0).equals(new_body_location));
+    }
 }
--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/BodyLocationTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/BodyLocationTest.java
@@ -1,5 +1,50 @@
+package ca.ualberta.cs.personal_condition_tracker;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class BodyLocationTest {
+public class BodyLocationTest extends TestCase {
 
+    public void testGetBody_part() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setBody_part("Right Hand");
+        assertTrue(body_loc.getBody_part().equals("Right Hand"));
+    }
+
+    public void testSetBody_part() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setBody_part("Right Hand");
+        assertTrue(body_loc.getBody_part().equals("Right Hand"));
+    }
+
+    public void testGetPhoto_coordinates() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setPhoto_x_coordinate(4);
+        body_loc.setPhoto_y_coordinate(3);
+        assertTrue((body_loc.getPhoto_x_coordinate() == 4) && (body_loc.getPhoto_y_coordinate() == 3));
+    }
+
+    public void testSetPhoto_coordinates() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setPhoto_x_coordinate(4);
+        body_loc.setPhoto_y_coordinate(3);
+        assertTrue((body_loc.getPhoto_x_coordinate() == 4) && (body_loc.getPhoto_y_coordinate() == 3));
+    }
+
+
+    public void testGetBody_coordinates() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setBody_x_coordinate(2);
+        body_loc.setBody_y_coordinate(1);
+        assertTrue((body_loc.getBody_x_coordinate() == 2) && (body_loc.getBody_y_coordinate() == 1));
+    }
+
+    public void testSetBody_coordinates() {
+        BodyLocation body_loc = new BodyLocation();
+        body_loc.setBody_x_coordinate(2);
+        body_loc.setBody_y_coordinate(1);
+        assertTrue((body_loc.getBody_x_coordinate() == 2) && (body_loc.getBody_y_coordinate() == 1));
+    }
 }
