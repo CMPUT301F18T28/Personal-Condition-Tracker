d1578f9d6422bc5f6cafdbb8b4cbb2b316d08427
Dray <draydon@ualberta.ca>
2018-11-01 22:22:50 -0600

    Finished adding tests for the Condition class as well as required signatures

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Condition.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Condition.java
@@ -8,8 +8,58 @@ public class Condition {
     private String title;
     private Date date;
     private String description;
-    private RecordList record_list;
-    private ArrayList<String> comment_list;
+    private RecordList recordList;
+    private ArrayList<String> commentList;
     private static final Integer MAX_CHARACTERS = 100;
 
+    Condition(String title, String description) {
+        
+    }
+    Condition(String title, Date date, String description, RecordList recordList, ArrayList<String> commentList) {
+        this.title = title;
+        this.date = date;
+        this.description = description;
+        this.recordList = recordList;
+        this.commentList = commentList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public RecordList getRecordList() {
+        return recordList;
+    }
+
+    public void setRecordList(RecordList recordList) {
+        this.recordList = recordList;
+    }
+
+    public ArrayList<String> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(ArrayList<String> commentList) {
+        this.commentList = commentList;
+    }
 }
