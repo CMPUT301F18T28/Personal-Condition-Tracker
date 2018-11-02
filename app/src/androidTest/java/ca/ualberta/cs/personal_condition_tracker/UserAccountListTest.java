73566c2d84a397022586aad62b29f627451653c3
William Nichols <wcomhelp@users.noreply.github.com>
2018-11-01 20:22:37 -0600

    Create the deleteUserAccount test
    Signed-off-by: William Nichols <wcomhelp@users.noreply.github.com>

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/UserAccountListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/UserAccountListTest.java
@@ -47,6 +47,39 @@ public class UserAccountListTest {
 
     @Test
     public void deleteUserAccount() {
+        UserAccountList test_user_account_list = new UserAccountList();
+
+        /** Creates a User Account to add to the UserAccountList */
+        UserAccount testUserAccount = new UserAccount();
+        testUserAccount.setName("Yang");
+        testUserAccount.setUserID("Yang");
+        testUserAccount.setEmail_address("Yang@gmail.com");
+        testUserAccount.setPassword("Yang'sPassword");
+
+        /** Create a ArrayList<UserAccount> called test_user_accounts to hold the actual values */
+        ArrayList<UserAccount> test_user_accounts = new ArrayList<UserAccount>();
+
+        /** Add the testUserAccount to the test_user_accounts ArrayList */
+        test_user_accounts.add(testUserAccount);
+
+        /** Add the testUserAccount to the test_user_account_list */
+        test_user_account_list.addUserAccount(testUserAccount);
+
+        /** Check that the test_user_account_list is equal to the test_user_accounts ArrayList */
+        assertEquals(test_user_account_list, test_user_accounts);
+
+        /** Remove testUserAccount from test_user_accounts */
+        test_user_accounts.remove(testUserAccount);
+
+        /** Check that the test_user_account_list does NOT equal to test_user_accounts */
+        assertFalse(test_user_account_list.equals(test_user_accounts));
+
+        /** Remove testUserAccount from test_user_account_list */
+        test_user_account_list.deleteUserAccount(testUserAccount);
+
+        /** Check that the test_user_account_list is equal to the test_user_accounts ArrayList */
+        assertEquals(test_user_account_list, test_user_accounts);
+
     }
 
     @Test
