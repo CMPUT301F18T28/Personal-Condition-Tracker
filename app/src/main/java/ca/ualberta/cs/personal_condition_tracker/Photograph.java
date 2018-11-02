a5928d956409ea315c7b1aa35993dfd89c878f42
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 19:50:50 -0600

    adding methods and attributes

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Photograph.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Photograph.java
@@ -2,7 +2,43 @@ package ca.ualberta.cs.personal_condition_tracker;
 
 import java.util.Base64;
 
-public class Photographs {
+public class Photograph {
     private String filename;
     private Base64 thumbnail;
+
+    Photograph() {
+        this.filename = "";
+        this.thumbnail = null;
+    }
+    Photograph(String new_filename) {
+        this.filename = new_filename;
+        this.thumbnail = null;
+    }
+
+    public String getFilename() {
+        return this.filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Base64 getThumbnail() {
+        return this.thumbnail;
+    }
+
+    public void setThumbnail(Base64 thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public void attachPhoto() {
+        // will be completed in Part 4
+    }
+    public void readFromDisk() {
+        // will be completed in Part 4
+    }
+    public void saveToDisk() {
+        // will be completed in Part 4
+    }
+
 }
