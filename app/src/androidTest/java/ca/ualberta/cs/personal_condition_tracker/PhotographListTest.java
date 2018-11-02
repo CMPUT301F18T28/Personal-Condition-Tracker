c465543a97fc131b190ea9ec1abd9e21b3341461
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 20:25:53 -0600

    modifying test cases

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographListTest.java
@@ -1,43 +1,45 @@
 package ca.ualberta.cs.personal_condition_tracker;
 
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-public class PhotographListTest {
+public class PhotographListTest extends TestCase{
 
-    public void testHasRecord(){
-        RecordList new_record_list = new RecordList();
-        Record new_record = new Record("Title",  "I am a record.");
-        new_record_list.addRecord(new_record);
-        assertTrue(new_record_list.hasRecord(new_record));
+    public void testHasPhotograph(){
+        PhotographList photos = new PhotographList();
+        Photograph photo = new Photograph();
+        photos.addPhotograph(photo);
+        assertTrue(photos.hasPhotograph(photo));
     }
 
-    public void testAddRecord() {
-        Record new_record = new Record("Title",  "I am a record.");
-        RecordList new_record_list = new RecordList();
-        new_record_list.addRecord(new_record);
-        assertTrue(new_record_list.hasRecord(new_record));
+    public void testAddPhotograph(){
+        PhotographList photos = new PhotographList();
+        Photograph photo = new Photograph();
+        photos.addPhotograph(photo);
+        assertTrue(photos.hasPhotograph(photo));
     }
 
     public void testDeleteRecord() {
-        RecordList new_record_list = new RecordList();
-        Record new_record = new Record("Title", "I am a record.");
-        new_record_list.addRecord(new_record);
-        new_record_list.deleteRecord(new_record);
-        assertFalse(new_record_list.hasRecord(new_record));
+        PhotographList photos = new PhotographList();
+        Photograph photo = new Photograph();
+        photos.addPhotograph(photo);
+        assertTrue(photos.hasPhotograph(photo));
+        photos.deletePhotograph(photo);
+        assertFalse(photos.hasPhotograph(photo));
     }
     public void testGetRecord() {
-        Record new_record = new Record("Title",  "I am a record.");
-        RecordList new_record_list = new RecordList();
-        new_record_list.addRecord(new_record);
-        assertTrue(new_record_list.getRecord(0).equals(new_record));
+        PhotographList photos = new PhotographList();
+        Photograph photo = new Photograph();
+        photos.addPhotograph(photo);
+        assertTrue(photos.getPhotograph(0).equals(photo));
     }
     public void testEditRecord() {
-        Record new_record = new Record("Title", "I am a record.");
-        RecordList new_record_list = new RecordList();
-        new_record_list.addRecord(new_record);
-        Record newer_record = new Record("Title",  "I am still a record.");
-        new_record_list.editRecord(0, newer_record);
-        assertTrue(new_record_list.getRecord(0).equals(newer_record));
+        PhotographList photos = new PhotographList();
+        Photograph photo = new Photograph();
+        photos.addPhotograph(photo);
+        Photograph new_photo = new Photograph("New Photograph");
+        photos.editPhotograph(0, new_photo);
+        assertTrue(photos.getPhotograph(0).equals(new_photo));
     }
 
 }
