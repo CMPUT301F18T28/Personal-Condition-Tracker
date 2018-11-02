27e54065b8668357e73954e00e6558db5eeaa6b5
Riley Voon <rcvoon@cs.ualberta.ca>
2018-11-01 19:50:30 -0600

    adding test cases

--- app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/personal_condition_tracker/PhotographTest.java
@@ -1,5 +1,24 @@
+package ca.ualberta.cs.personal_condition_tracker;
+
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-public class PhotographTest {
+import java.util.Base64;
 
+public class PhotographTest extends TestCase {
+
+
+    public void testGetFilename() {
+        Photograph photo = new Photograph();
+        photo.setFilename("Photograph");
+        assertTrue(photo.getFilename().equals("Photograph"));
+    }
+    public void testSetFilename() {
+        Photograph photo = new Photograph("Photograph");
+        assertTrue(photo.getFilename().equals("Photograph"));
+        photo.setFilename("StillAPhotograph");
+        assertTrue(photo.getFilename().equals("StillAPhotograph"));
+    }
+    public void textGetThumbnail() {
+    }
 }
