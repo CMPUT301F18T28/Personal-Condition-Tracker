46c3cee9dc66393e3065c4435369ec5010cbd1f2
Dray <draydon@ualberta.ca>
2018-11-18 16:55:00 -0700

    Care Provider can now remove Patient ID's from their list.

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ViewPatientListActivity.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ViewPatientListActivity.java
@@ -1,12 +1,15 @@
 package ca.ualberta.cs.personal_condition_tracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,6 +20,7 @@ import java.util.Collection;
 public class ViewPatientListActivity extends AppCompatActivity {
     private UserAccountListController userAccountListController = new UserAccountListController();
     private CareProvider activeCareProvider = userAccountListController.getUserAccountList().getActiveCareProvider();
+    private String selectedPatientID;
 
 
     @Override
@@ -39,6 +43,36 @@ public class ViewPatientListActivity extends AppCompatActivity {
                 Collection<String> patientIDCollection = activeCareProvider.getPatientList().getPatientIDs();
                 patientIDs.addAll(patientIDCollection);
                 patientIDArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        //Ugly code of OnItemLongClickListener
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder adb = new AlertDialog.Builder(ViewPatientListActivity.this);
+                adb.setMessage("Would you like remove " + patientIDs.get(position).toString() + "?");
+                adb.setCancelable(true);
+                final int finalPosition = position;
+
+                adb.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectedPatientID= patientIDs.get(finalPosition);
+                        activeCareProvider.getPatientList().deletePatient(selectedPatientID);
+                    }
+                });
+
+
+                adb.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Do nothing, simply allow the dialog to close
+                    }
+                });
+
+                adb.show();
+                return false;
             }
         });
     }
