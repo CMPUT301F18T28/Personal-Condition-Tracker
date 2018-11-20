e9d453c81beda0ccf3b13949912e5122e7489214
Damian Douziech <douziech@ualberta.ca>
2018-11-19 17:04:29 -0700

    "Javadoc for the ConditionList class and all of it's methods."

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ConditionList.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ConditionList.java
@@ -22,7 +22,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 
-
+/**
+ * Serves to store both an ArrayList of Condition objects and an ArrayList of Listeners, thus providing access, setting,
+ * getting and modification to the former and monitoring via the latter. In the case of a Care Provider, a conditionOfInterest attribute
+ * provides for user selection from the respective list.
+ *
+ * @author      D. Douziech
+ * @author      D. Buksa
+ * @version     1.1, 11-18-18
+ * @since       1.0
+ */
 
 
 public class ConditionList{
@@ -31,14 +40,34 @@ public class ConditionList{
     private transient ArrayList<Listener> listenerList = null;
     private Condition conditionOfInterest = null;
 
+
     ConditionList(){
         this.condition_list = new ArrayList<Condition>();
     }
+
+    /**
+     *Simply appends a Condition object to the existing  list of conditions.
+     *
+     * <P>
+     * @param condition A Condition object to be added to the list.
+     * @return Nothing
+     * @see Condition
+     */
 
     public void addCondition(Condition condition){
         condition_list.add(condition);
         notifyListeners();
     }
+
+
+    /**
+     * Removes a specified condition from the list thereof.
+     *
+     * <P>
+     * @param condition A Condition object to be removed from the list.
+     * @return boolean True if the condition has been removed or false otherwise.  An error message
+     * logged if the specified condition does not exist.
+     */
 
 
     public boolean deleteCondition(Condition condition) {
@@ -53,6 +82,18 @@ public class ConditionList{
         return boolResult;
     }
 
+
+    /**
+     * Provides the ability to edit three of the attributes of a Condition object: title, date and description .
+     *
+     * <P>
+     * @param condition Condition object to be modified
+     * @param title title given to a condition
+     * @param date Date object specifying the date a condition was logged
+     * @param description a description of the condition
+     * @return Nothing
+     *
+     */
 
     //Edit one or all of these three: title, date, description...
     public void editCondition(Condition condition, String title, Date date, String description){
@@ -70,6 +111,22 @@ public class ConditionList{
             condition.setDescription(description);
         }
     }
+
+
+
+    /**
+     * Provides the ability to edit all of the attributes of a Condition object except for the constant that is used to restrict the input length of String parameters.
+     * <P>
+     * @param condition Condition object to be modified
+     * @param title title given to a condition
+     * @param date Date object specifying the date a condition was logged
+     * @param description a description of the condition
+     * @param recordList an ArrayList of Record objects representing entries pertaining to a condition
+     * @param commentList an ArrayList of comments; supplied by Care Providers in response to patient conditions
+     * @return Nothing
+     * @see Record
+     * @see RecordList
+     */
 
     //Edit the whole shebang...
     public void editCondition(Condition condition, String title, Date date, String description, RecordList recordList, ArrayList<String> commentList){
@@ -91,6 +148,14 @@ public class ConditionList{
     }
 
 
+
+    /**
+     * Verifies the existence of a supplied condition for a particular patient.
+     * <P>
+     * @param condition Condition object to requiring verification
+     * @return Condition The same object supplied for testing otherwise an error message is simply logged.
+     */
+
     //This method is strictly a search using an actual Condition object.  Additional methods for searching the list of Conditions by
     // description, etc. may have to be added later.
 
@@ -111,9 +176,25 @@ public class ConditionList{
         return foundCondition;
     }
 
+
+    /**
+     * Provides the number of conditions currently recorded for a specific patient.
+     * <P>
+     * @return int size of the list of conditions.
+     */
+
+
     public int sizeOfList(){
         return this.condition_list.size();
     }
+
+
+    /**
+     * Sorts the list of conditions chronologically by date.
+     * <P>
+     * @return Nothing
+     * @see java.util.Date
+     */
 
 
     public void sortByDate(){
@@ -122,11 +203,24 @@ public class ConditionList{
     }
 
 
+    /**
+     * Prints, in the form of a log entry, the list of conditions chronologically, by date.
+     * <P>
+     * @return Nothing
+     */
+
+
     public void printListByDate(){
         for(Condition condition : condition_list)
             // System.out.println("Date: " + condition.getDate());
             Log.d("Date: ", condition.getDate().toString());
     }
+
+    /**
+     * Sorts the list of conditions chronologically by date.
+     * <P>
+     * @return Nothing
+     */
 
     public int getIndex(Condition condition){
         int index = -1;
@@ -136,22 +230,64 @@ public class ConditionList{
         return index;
     }
 
+
+
+    /**
+     * Obtains the condition from the list of conditions according to a supplied index.
+     * <P>
+     * @return Condition
+     * @throws IndexOutOfBoundsException when the index < 0 || index >= condition_list.size()
+     * @see IndexOutOfBoundsException
+     */
+
     public Condition getByIndex(int index){
         Condition condition = condition_list.get(index);
         return condition;
     }
 
+    /**
+     * Provides the list of conditions, that is, the condition_list attribute.
+     * <P>
+     * @return ArrayList<Condition>
+     */
+
     public ArrayList<Condition> getConditions(){
         return condition_list;
     }
+
+
+    /**
+     * Appends a Listener object to the list thereof.
+     * <P>
+     * @param listener Listener object
+     * @return Nothing
+     * @see Listener
+     */
 
     public void addListener(Listener listener){
         getListenerList().add(listener);
     }
 
+
+    /**
+     * Removes a given Listener object from the list thereof.
+     * <P>
+     * @param listener Listener object
+     * @return Nothing
+     * @see Listener
+     */
+
     public void removeListener(Listener listener) {
         getListenerList().remove(listener);
     }
+
+
+    /**
+     * Obtains the list of Listeners, that is, the listenerList attribute. If called for the first time
+     * this method serves to initialize the list.
+     * <P>
+     * @return ArrayList<Listener>
+     */
 
     private ArrayList<Listener> getListenerList(){
         if(listenerList == null){
@@ -160,15 +296,37 @@ public class ConditionList{
         return listenerList;
     }
 
+    /**
+     * Serves to update all Listener objects contained in the list thereof.
+     * <P>
+     * @return Nothing
+     * @see Listener
+     */
+
     public void notifyListeners(){
         for(Listener listener: getListenerList()){
             listener.update();
         }
     }
 
+    /**
+     * Provides the conditionOfInterest attribute; a particular patient condition that has been selected.
+     * <P>
+     * @return Condition
+     * @see Condition
+     */
+
     public Condition getConditionOfInterest() {
         return conditionOfInterest;
     }
+
+
+    /**
+     * Registers a particular patient condition as selected by the user.
+     * <P>
+     * @param conditionOfInterest Condition object
+     * @return Nothing
+     */
 
     public void setConditionOfInterest(Condition conditionOfInterest) {
         this.conditionOfInterest = conditionOfInterest;
