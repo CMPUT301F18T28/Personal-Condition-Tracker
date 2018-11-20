787b68f624feccf311bfa125c02cd67c787e3fc6
Damian Douziech <douziech@ualberta.ca>
2018-11-19 21:17:34 -0700

    Javadoc for the CareProvider class and its methods.

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/CareProvider.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/CareProvider.java
@@ -1,12 +1,46 @@
 package ca.ualberta.cs.personal_condition_tracker;
 
+
+/**
+ * The CareProvider class is used to represent all of the account information pertaining to Care Providers.
+ * @author      C. Neureuter
+ * @version     1.1, 11-18-18
+ * @since       1.0
+ */
+
 public class CareProvider extends UserAccount{
-    private PatientList patient_list;
+
+    private PatientList patientList = new PatientList();
+
     CareProvider() {
         super();
     }
 
-    public CareProvider(String accountType, String userID, String emailAddress, String password) {
+    CareProvider(String accountType, String userID, String emailAddress, String password) {
         super(accountType,userID,emailAddress,password);
     }
+
+    /**
+     * Provides the list of Patient objects representing the patients that are assigned to a particular Care Provider.
+     * @return  patientList List of patient that are assigned to a particular Care Provider
+     * @see PatientList
+     * @see Patient
+     */
+    public PatientList getPatientList() {
+        return patientList;
+    }
+
+
+    /**
+     * Sets the list of patients assigned to a particular Care Provider.
+     * @param patientList List of patients, that is, a list of Patient objects
+     * @return  Nothing
+     * @see PatientList
+     * @see Patient
+     */
+
+    public void setPatientList(PatientList patientList) {
+        this.patientList = patientList;
+    }
 }
+
