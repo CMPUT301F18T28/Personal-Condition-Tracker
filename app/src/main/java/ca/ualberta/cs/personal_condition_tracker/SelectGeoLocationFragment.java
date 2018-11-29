b12904723f8fe2c3411d55f7248dbebe9e4f82dd
William Nichols <wcomhelp@users.noreply.github.com>
2018-11-29 15:55:05 -0700

    Add Geolocation functionality for Part5.
    Signed-off-by: William Nichols <wcomhelp@users.noreply.github.com>

---
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/SelectGeoLocationFragment.java
@@ -0,0 +1,74 @@
+package ca.ualberta.cs.personal_condition_tracker;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SelectGeoLocationFragment extends Fragment implements OnMapReadyCallback {
+
+    MapView mapView;
+    GoogleMap map;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_select_geo_location, container, false);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this); // get the map from google
+
+
+        return view;
+    }
+
+    @SuppressLint("MissingPermission") // The permission is required in the manifest
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.setMyLocationEnabled(true);
+        /* Add a marker in Sydney, Australia,
+           and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        map.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
