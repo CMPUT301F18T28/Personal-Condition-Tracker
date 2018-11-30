fa0ab70440e03a79072f43612464ce86e0237f75
William Nichols <wcomhelp@users.noreply.github.com>
2018-11-29 22:03:20 -0700

    Create the viewGeoLocation method to change intent
    Signed-off-by: William Nichols <wcomhelp@users.noreply.github.com>

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ViewRecordActivity.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ViewRecordActivity.java
@@ -51,6 +51,7 @@ package ca.ualberta.cs.personal_condition_tracker;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -82,8 +83,11 @@ public class ViewRecordActivity extends Activity {
         recordDateView.setText(recordDate);
         recordDescriptionView.setText(recordDescription);
     }
-    public void viewGeoLocations(View v){
-        Toast.makeText(this,"View geo locations", Toast.LENGTH_SHORT).show();
+    public void viewGeoLocation(View v) {
+        Toast.makeText(this,"View geo location", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(ViewRecordActivity.this, MapsActivity.class);
+        intent.putExtra("mapMode", "view");
+        startActivity(intent);
     }
 
     public void viewBodyLocations(View v){
