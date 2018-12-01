76827db82e85527af50426d5b7943fe680ddf36d
William Nichols <wcomhelp@users.noreply.github.com>
2018-11-30 23:41:00 -0700

    Remove unneeded import of Button, as we don't use this class.
    Signed-off-by: William Nichols <wcomhelp@users.noreply.github.com>

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ModifyRecordActivity.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/ModifyRecordActivity.java
@@ -34,7 +34,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
