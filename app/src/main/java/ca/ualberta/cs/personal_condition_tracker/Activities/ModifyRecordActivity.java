780b3b4ce81ac01a94e14988ee6fee089e3afe38
William Nichols <wcomhelp@users.noreply.github.com>
2018-12-03 15:05:54 -0700

    Update ModifyRecordActivity.java

--- app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Activities/ModifyRecordActivity.java
+++ app/src/main/java/ca/ualberta/cs/personal_condition_tracker/Activities/ModifyRecordActivity.java
@@ -1,15 +1,3 @@
-/* Personal Condition Tracker : A simple and attractive Android application that allows an individual to
- document, track and review the progression of a personal health issue (a 'condition'), thus serving to facilitate
- enhanced clarity of communicating between patient and care provider, early detection and accurate prognosis with the
- aim of obtaining medical treatment as soon as possible.
- Document the facts - get the treatment you deserve!
- Copyright (C) 2018
- R. Voon; rcvoon@ualberta.ca
- D. Buksa; draydon@ualberta.ca
- W. Nichols; wnichols@ualberta.ca
- D. Douziech; douziech@ualberta.ca
- C. Neureuter; neureute@ualberta.ca
-*/
 package ca.ualberta.cs.personal_condition_tracker.Activities;
 
 import android.app.Activity;
@@ -62,7 +50,7 @@ import static ca.ualberta.cs.personal_condition_tracker.PermissionRequest.verify
  * added or editted.
  * @author    R. Voon; rcvoon@ualberta.ca
  * @author    D. Buksa; draydon@ualberta.ca
- * @author    D. Douziech; douziech@ualberta.ca
+ * @author    W. Nichols; wnichols@ualberta.ca
  * @version   1.1, 11-18-18
  * @since     1.0
  */
